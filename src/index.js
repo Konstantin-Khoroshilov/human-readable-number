57106f7e55a2141bfee2cf975269539cab2591f6
Konstantin Khoroshilov <manly41142009@yandex.ru>
2024-10-26 13:44:19 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+  const dictionary = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+    20: 'twenty',
+    30: 'thirty',
+    40: 'forty',
+    50: 'fifty',
+    60: 'sixty',
+    70: 'seventy',
+    80: 'eighty',
+    90: 'ninety',
+    100: 'hundred',
+  }
+
+  if (number <= 20) {
+    return dictionary[number];
+  }
+
+  if (number < 100) {
+    return `${dictionary[number % 100 - number % 100 % 10]}${number % 100 % 10 != 0 ? ' ' + dictionary[number % 100 % 10] : ''}`;
+  }
+
+  if (number >= 100 && number < 1000) {
+    return `${dictionary[Math.floor(number / 100)]} ${dictionary[100]}${number % 100 != 0 ? ' ' + toReadable(number % 100) : ''}`;
+  }
 }
